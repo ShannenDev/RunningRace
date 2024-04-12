976ef85c10a35f6e07b93d8af2c6690f1687d2c9
petra <petraviola.beck@gmail.com>
2024-04-12 14:43:03 +0100

    Create initial structure - entities, DTOs, controllers, services and repositories

--- src/main/java/ie/shannen/runningrace/repository/RaceRepository.java
+++ src/main/java/ie/shannen/runningrace/repository/RaceRepository.java
@@ -2,9 +2,11 @@ package ie.shannen.runningrace.repository;
 
 import ie.shannen.runningrace.repository.model.RaceEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RaceRepository extends CrudRepository<RaceEntity, UUID> {
 
 }
--- src/main/java/ie/shannen/runningrace/repository/ResultRepository.java
+++ src/main/java/ie/shannen/runningrace/repository/ResultRepository.java
@@ -2,9 +2,11 @@ package ie.shannen.runningrace.repository;
 
 import ie.shannen.runningrace.repository.model.ResultEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ResultRepository extends CrudRepository<ResultEntity, UUID> {
 
 }
