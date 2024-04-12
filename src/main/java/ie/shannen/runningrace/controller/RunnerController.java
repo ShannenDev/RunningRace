1e5c30cc92def1a40661d0588eb9e9162a3faa60
petra <petraviola.beck@gmail.com>
2024-04-12 14:43:03 +0100

    Create initial structure - entities, DTOs, controllers, services and repositories

--- src/main/java/ie/shannen/runningrace/controller/RunnerController.java
+++ src/main/java/ie/shannen/runningrace/controller/RunnerController.java
@@ -1,7 +1,16 @@
 package ie.shannen.runningrace.controller;
 
+import ie.shannen.runningrace.service.RunnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class RunnerController {
+
+    private RunnerService runnerService;
+
+    @Autowired
+    public RunnerController(RunnerService runnerService) {
+        this.runnerService = runnerService;
+    }
 }
